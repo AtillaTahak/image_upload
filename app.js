2a499ec24a0927e211e72372eeaa097d708dbc66
atillatahak <atillatahaa@gmail.com>
2022-08-29 02:38:15 +0300

    created files and added style for index file

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ import express from 'express'
 
 const app = express();
 const port = 3000;
+//ejs template engine
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+//Static files are served from the public folder
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
